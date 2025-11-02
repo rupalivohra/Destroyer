e1cc7f71672603a61214839f959b6fe65bfb6713
Rupali Vohra <rupali2vohra@gmail.com>
2025-11-02 02:28:07 -0800

    Move ship placement to TS

--- placement.ts
+++ placement.ts
@@ -1,11 +1,10 @@
-import { Direction } from "./enums.js";
+import { Direction, PlayerType } from "./enums.js";
 
-export function getCells(startCell: number, shipDirection: Direction, shipSize: number, prefix: string | null, playerGrid: any, computerGrid: any): number[] {
+export function getCells(startCell: number, shipDirection: Direction, shipSize: number, playerType: PlayerType | null, playerGrid: any, computerGrid: any): number[] {
     /* startCell is the head of the ship
     shipDirection is the direction relative to the head the ship should be placed
     shipSize is the number of units the desired ship should have
     This function returns a list of cell numbers in which the ship should be placed.*/
-    console.log("getCells called with startCell: " + startCell + ", shipDirection: " + shipDirection + ", shipSize: " + shipSize + ", prefix: " + prefix);
     let shipPlacementCells: number[] = [];
     shipPlacementCells.push(startCell);
     var location = startCell;
@@ -35,8 +34,8 @@ export function getCells(startCell: number, shipDirection: Direction, shipSize: 
             location = location - 9;
             shipPlacementCells.push(location);
         }
-        if (prefix != null) {
-            if (checkEmpty(location, prefix, playerGrid, computerGrid) === false) { /*check to make sure the cells are empty*/
+        if (playerType != null) {
+            if (checkEmpty(location, playerType, playerGrid, computerGrid) === false) { /*check to make sure the cells are empty*/
                 //alert("the cell was not empty");
                 shipPlacementCells = [];
                 break;
@@ -47,10 +46,10 @@ export function getCells(startCell: number, shipDirection: Direction, shipSize: 
     return shipPlacementCells;
 }
 
-function checkEmpty(cell: number, prefix: string | null, playerGrid: any, computerGrid: any): boolean {
+export function checkEmpty(cell: number, playerType: PlayerType | null, playerGrid: any, computerGrid: any): boolean {
     /* cell is the number of the cell to check.
     returns True if the cell is empty. returns False if the cell is occupied.*/
-    if (prefix == "p") {
+    if (playerType == PlayerType.Player) {
         if (playerGrid[cell].ship == null) {
             return true;
         }
